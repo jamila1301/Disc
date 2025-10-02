30e1570bd792d25b94cc57aeaee4e26f20eef4da
jamila1301 <cemilemehemmedli@gmail.com>
2025-10-02 23:29:21 +0400

    add custom font

---
+++ Disc/Extensions/UIFont+.swift
@@ -0,0 +1,32 @@
+//
+//  UIFont+.swift
+//  Disc
+//
+//  Created by Jamila Mahammadli on 02.10.25.
+//
+
+import UIKit
+
+extension UIFont {
+    static let plusJakartaSans36 = UIFont(name: PlusJakartaSansFont.semibold, size: 36)
+    static let plusJakartaSans14 = UIFont(name: PlusJakartaSansFont.regular, size: 14)
+    static let plusJakartaSans16 = UIFont(name: PlusJakartaSansFont.semibold, size: 16)
+}
+
+fileprivate enum PlusJakartaSansFont {
+    static let bold = "PlusJakartaSans-Bold"
+    static let boldItalic = "PlusJakartaSans-BoldItalic"
+    static let extraBold = "PlusJakartaSans-ExtraBold"
+    static let extraBoldItalic = "PlusJakartaSans-ExtraBoldItalic"
+    static let extraLight = "PlusJakartaSans-ExtraLight"
+    static let extraLightItalic = "PlusJakartaSans-ExtraLightItalic"
+    static let italic = "PlusJakartaSans-Italic"
+    static let light = "PlusJakartaSans-Light"
+    static let lightItalic = "PlusJakartaSans-LightItalic"
+    static let medium = "PlusJakartaSans-Medium"
+    static let mediumItalic = "PlusJakartaSans-MediumItalic"
+    static let regular = "PlusJakartaSans-Regular"
+    static let semibold = "PlusJakartaSans-SemiBold"
+    static let semiboldItalic = "PlusJakartaSans-SemiBoldItalic"
+    
+}
