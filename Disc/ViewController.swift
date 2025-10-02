ca2a660bff8392188a54bb5db1b2a329d25df4dd
jamila1301 <cemilemehemmedli@gmail.com>
2025-10-02 21:17:10 +0400

    Initial Commit

---
+++ Disc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Disc
+//
+//  Created by Jamila Mahammadli on 02.10.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
