9cb0fd165335a27a5ec7abd4cf838e5022d41ec5
jamila1301 <cemilemehemmedli@gmail.com>
2025-10-03 15:10:17 +0400

    add onboarding screen

---
+++ Disc/Features/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,107 @@
+//
+//  OnboardingViewController.swift
+//  Disc
+//
+//  Created by Jamila Mahammadli on 02.10.25.
+//
+
+import UIKit
+import SnapKit
+
+class OnboardingViewController: UIViewController {
+    
+    private let mainImageView: UIImageView = {
+        let v = UIImageView()
+        v.image = .onboarding
+        v.contentMode = .scaleAspectFit
+        return v
+    }()
+    
+    private let titleLabel: UILabel = {
+        let v = UILabel()
+        v.text = "Discover Latest Music and Podcast"
+        v.numberOfLines = .zero
+        v.font = UIFont.plusJakartaSans36
+        v.textAlignment = .center
+        return v
+    }()
+    
+    private let subtitleLabel: UILabel = {
+        let v = UILabel()
+        v.text = "Listen to the latest music and podcast releases\nusing Disc. app"
+        v.numberOfLines = .zero
+        v.font = UIFont.plusJakartaSans14
+        v.textAlignment = .center
+        v.textColor = .lightGrayPrimary
+        return v
+    }()
+    
+    private lazy var startButton: UIButton = {
+        let v = UIButton(type: .system)
+        v.setTitle("Start Exploring", for: .normal)
+        v.titleLabel?.font = UIFont.plusJakartaSans16
+        v.setTitleColor(.white, for: .normal)
+        v.backgroundColor = .defaultBlue
+        v.layer.cornerRadius = 8
+        v.addTarget(self, action: #selector(didTapStartButton), for: .touchUpInside)
+        return v
+    }()
+    
+    private let labelsStackView: UIStackView = {
+        let v = UIStackView()
+        v.axis = .vertical
+        v.spacing = 16
+        v.alignment = .fill
+        return v
+    }()
+    
+    private let mainStackView: UIStackView = {
+        let v = UIStackView()
+        v.axis = .vertical
+        v.spacing = 32
+        v.alignment = .fill
+        return v
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .screenBackground
+        
+        [titleLabel, subtitleLabel].forEach { v in
+            labelsStackView.addArrangedSubview(v)
+        }
+        
+        [mainImageView, labelsStackView].forEach { v in
+            mainStackView.addArrangedSubview(v)
+        }
+        
+        [mainStackView, startButton].forEach { v in
+            view.addSubview(v)
+        }
+        
+        mainStackView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(32)
+            make.horizontalEdges.equalToSuperview().inset(24)
+            make.bottom.lessThanOrEqualTo(startButton.snp.top).offset(-24)
+        }
+        
+        mainImageView.snp.makeConstraints { make in
+            make.height.lessThanOrEqualTo(view.snp.height).multipliedBy(0.45)
+            make.height.greaterThanOrEqualTo(150)
+        }
+        
+        startButton.snp.makeConstraints { make in
+            make.height.equalTo(52)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(24)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+    }
+    
+    @objc
+    private func didTapStartButton() {}
+}
+
